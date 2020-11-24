1b95b3034ef97a6b70b1b7524389cb209442e696
diego garcia <diegodenavas@gmail.com>
2020-11-24 17:27:14 +0100

    codigo optimizado

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     
+    //Declaración de variables necesarias
     var array1 = ["aguacate", "ajo", "cebolla", "pepino", "puerro", "tomate", "zanahoria"];
     var array2 = ["aguacate", "ajo", "cebolla", "pepino", "puerro", "tomate", "zanahoria"];
     var array3 = ["aguacate", "ajo", "cebolla", "pepino", "puerro", "tomate", "zanahoria"];
@@ -22,9 +23,9 @@ window.onload = function() {
     let num3 = random();
 
     cargarSlots(num1, num2, num3);
-    animarTirar();
+    animarBotones();
 
-    function animarTirar(){
+    function animarBotones(){
         botonTirar.animate([
             // keyframes
             { backgroundColor: "red"}
@@ -101,32 +102,32 @@ window.onload = function() {
 
         if (numero1 == 6 && numero2 == 6 && numero3 == 6) {
             monedas += 10; //tres zanahorias
-            mensaje3zanahorias();
+            mensajePremio(6);
         }
 
         else if (numero1 == numero2 && numero2 == numero3) {
             monedas += 5; //tres hortalizas iguales
-            mensaje3horta();
+            mensajePremio(5);
         }
 
         else if ((numero1 == 6 && numero2 == 6) || (numero1 == 6 && numero3 == 6) || (numero2 == 6 && numero3 == 6)) {
             monedas += 4; //dos zanahorias
-            mensaje2zanahorias();
+            mensajePremio(4);
         }
 
         else if ((numero1 == 6 || numero2 == 6 || numero3 == 6) && (numero1 == numero2 || numero1 == numero3 || numero2 == numero3)) {
             monedas += 3; //una zanahoria y dos hortalizas iguales
-            mensajeZana2horta();
+            mensajePremio(3);
         }
 
         else if (numero1 == numero2 || numero1 == numero3 || numero2 == numero3) {
             monedas += 2; //dos hortalizas iguales
-            mensaje2horta();
+            mensajePremio(2);
         }
 
         else if (numero1 == 6 || numero2 == 6 || numero3 == 6) {
             monedas += 1; //una zanahoria
-            mensajeZana();
+            mensajePremio(1);
         }
     }
 
@@ -152,50 +153,54 @@ window.onload = function() {
     var iconoMensajeVictoria = document.getElementById("iconoMensajeVictoria");
 
     //funciones para los mensajes
-    function mensaje3zanahorias(){
-        contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/3zanahorias/fondo3zanahorias.jpg')";
-        iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/3zanahorias/estrellatriplezanahoria.png')"
-        botonTirar.setAttribute("disabled", "");
-        iconoMensajeVictoria.style.color = "black";
+    function mensajePremio(opcion){
 
-        contenedorMensajes.animate([
-            // keyframes
-            { backgroundImage: "url('imagenes/mensajesVictoria/3zanahorias/fondo2triplezanahoria.jpg')" }
-        ], { 
-            duration: 100,
-            iterations: 50,
-            direction: "alternate-reverse"
-        });
+        switch(opcion){
 
-        iconoMensajeVictoria.animate([
-            // keyframes
-            { backgroundSize: "0px 0px"},
-            { backgroundSize: "120px 120px" }
-        ], { 
-            duration: 1000,
-            easing: "ease",
-            fill: "forwards",
-        });
+            case 1:
+                contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoria/fondozana.jpg')";
+                iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoria/iconozana.png')"
+                botonTirar.setAttribute("disabled", "");
+                iconoMensajeVictoria.style.color = "black";
+                break;
 
-        setTimeout(function() {
-            iconoMensajeVictoria.innerHTML = "+10";
-        }, 1700);
+            case 2:
+                contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/2hortalizas/fondo2horta.jpg')";
+                iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/2hortalizas/icono2hortalizas.png')"
+                botonTirar.setAttribute("disabled", "");
+                iconoMensajeVictoria.style.color = "white";
+                break;
 
-        setTimeout(function() {
-            iconoMensajeVictoria.style.backgroundImage = "none";
-            iconoMensajeVictoria.innerHTML = "";
-            contenedorMensajes.style.backgroundImage = "none";
-            botonTirar.removeAttribute("disabled");
-        }, 5000);
-    }
+            case 3:
+                contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoriay2hortalizas/fondo1zana2horta.jpg')";
+                iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoriay2hortalizas/iconozana2horta.png')"
+                botonTirar.setAttribute("disabled", "");
+                iconoMensajeVictoria.style.color = "white";
+                break;
 
+            case 4:
+                contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/2zanahorias/fondo2zana.jpg')";
+                iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/2zanahorias/icono2zana.png')"
+                botonTirar.setAttribute("disabled", "");
+                iconoMensajeVictoria.style.color = "black";
+                break;
 
+            case 5:
+                contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/3hortalizas/fondo3horta.jpg')";
+                iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/3hortalizas/icono3hortalizas.png')"
+                botonTirar.setAttribute("disabled", "");
+                iconoMensajeVictoria.style.color = "black";
+                break;
 
-    function mensajeZana2horta(){
-        contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoriay2hortalizas/fondo1zana2horta.jpg')";
-        iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoriay2hortalizas/iconozana2horta.png')"
-        botonTirar.setAttribute("disabled", "");
-        iconoMensajeVictoria.style.color = "white";
+            case 6:
+                contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/3zanahorias/fondo3zanahorias.jpg')";
+                iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/3zanahorias/estrellatriplezanahoria.png')"
+                botonTirar.setAttribute("disabled", "");
+                iconoMensajeVictoria.style.color = "black";
+                break;
+
+        }
+
 
         contenedorMensajes.animate([
             // keyframes
@@ -217,7 +222,7 @@ window.onload = function() {
         });
 
         setTimeout(function() {
-            iconoMensajeVictoria.innerHTML = "+3";
+            iconoMensajeVictoria.innerHTML = "+" + opcion;
         }, 1700);
 
         setTimeout(function() {
@@ -226,159 +231,7 @@ window.onload = function() {
             contenedorMensajes.style.backgroundImage = "none";
             botonTirar.removeAttribute("disabled");
         }, 5000);
-    }
 
-
-
-    function mensajeZana(){
-        contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoria/fondozana.jpg')";
-        iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoria/iconozana.png')"
-        botonTirar.setAttribute("disabled", "");
-        iconoMensajeVictoria.style.color = "black";
-
-        contenedorMensajes.animate([
-            // keyframes
-            { backgroundImage: "url('imagenes/mensajesVictoria/1zanahoria/fondo2zana.jpg')" }
-        ], { 
-            duration: 100,
-            iterations: 50,
-            direction: "alternate-reverse"
-        });
-
-        iconoMensajeVictoria.animate([
-            // keyframes
-            { backgroundSize: "0px 0px"},
-            { backgroundSize: "120px 120px" }
-        ], { 
-            duration: 1000,
-            easing: "ease",
-            fill: "forwards",
-        });
-
-        setTimeout(function() {
-            iconoMensajeVictoria.innerHTML = "+1";
-        }, 1700);
-
-        setTimeout(function() {
-            iconoMensajeVictoria.style.backgroundImage = "none";
-            iconoMensajeVictoria.innerHTML = "";
-            contenedorMensajes.style.backgroundImage = "none";
-            botonTirar.removeAttribute("disabled");
-        }, 5000);
-    }
-
-
-    function mensaje2zanahorias(){
-        contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/2zanahorias/fondo2zana.jpg')";
-        iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/2zanahorias/icono2zana.png')"
-        botonTirar.setAttribute("disabled", "");
-        iconoMensajeVictoria.style.color = "black";
-
-        contenedorMensajes.animate([
-            // keyframes
-            { backgroundImage: "url('imagenes/mensajesVictoria/2zanahorias/fondo2zana2.jpg')" }
-        ], { 
-            duration: 100,
-            iterations: 50,
-            direction: "alternate-reverse"
-        });
-
-        iconoMensajeVictoria.animate([
-            // keyframes
-            { backgroundSize: "0px 0px"},
-            { backgroundSize: "120px 120px" }
-        ], { 
-            duration: 1000,
-            easing: "ease",
-            fill: "forwards",
-        });
-
-        setTimeout(function() {
-            iconoMensajeVictoria.innerHTML = "+4";
-        }, 1700);
-
-        setTimeout(function() {
-            iconoMensajeVictoria.style.backgroundImage = "none";
-            iconoMensajeVictoria.innerHTML = "";
-            contenedorMensajes.style.backgroundImage = "none";
-            botonTirar.removeAttribute("disabled");
-        }, 5000);
-    }
-
-
-    function mensaje3horta(){
-        contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/3hortalizas/fondo3horta.jpg')";
-        iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/3hortalizas/icono3hortalizas.png')"
-        botonTirar.setAttribute("disabled", "");
-        iconoMensajeVictoria.style.color = "black";
-
-        contenedorMensajes.animate([
-            // keyframes
-            { backgroundImage: "url('imagenes/mensajesVictoria/3hortalizas/fondo3horta2.jpg')" }
-        ], { 
-            duration: 100,
-            iterations: 50,
-            direction: "alternate-reverse"
-        });
-
-        iconoMensajeVictoria.animate([
-            // keyframes
-            { backgroundSize: "0px 0px"},
-            { backgroundSize: "120px 120px" }
-        ], { 
-            duration: 1000,
-            easing: "ease",
-            fill: "forwards",
-        });
-
-        setTimeout(function() {
-            iconoMensajeVictoria.innerHTML = "+5";
-        }, 1700);
-
-        setTimeout(function() {
-            iconoMensajeVictoria.style.backgroundImage = "none";
-            iconoMensajeVictoria.innerHTML = "";
-            contenedorMensajes.style.backgroundImage = "none";
-            botonTirar.removeAttribute("disabled");
-        }, 5000);
-    }
-
-
-    function mensaje2horta(){
-        contenedorMensajes.style.backgroundImage = "url('imagenes/mensajesVictoria/2hortalizas/fondo2horta.jpg')";
-        iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/2hortalizas/icono2hortalizas.png')"
-        botonTirar.setAttribute("disabled", "");
-        iconoMensajeVictoria.style.color = "white";
-
-        contenedorMensajes.animate([
-            // keyframes
-            { backgroundImage: "url('imagenes/mensajesVictoria/2hortalizas/fondo2horta2.jpg')" }
-        ], { 
-            duration: 100,
-            iterations: 50,
-            direction: "alternate-reverse"
-        });
-
-        iconoMensajeVictoria.animate([
-            // keyframes
-            { backgroundSize: "0px 0px"},
-            { backgroundSize: "120px 120px" }
-        ], { 
-            duration: 1000,
-            easing: "ease",
-            fill: "forwards",
-        });
-
-        setTimeout(function() {
-            iconoMensajeVictoria.innerHTML = "+2";
-        }, 1700);
-
-        setTimeout(function() {
-            iconoMensajeVictoria.style.backgroundImage = "none";
-            iconoMensajeVictoria.innerHTML = "";
-            contenedorMensajes.style.backgroundImage = "none";
-            botonTirar.removeAttribute("disabled");
-        }, 5000);
     }
 
 }
