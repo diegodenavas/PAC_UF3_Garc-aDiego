30a4ed534d9c8e67a52e0f9211e564b36e3442ea
diego garcia <diegodenavas@gmail.com>
2020-11-24 21:01:40 +0100

    version 1.0

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ window.onload = function() {
     var botonSalir = document.getElementById("botonSalir");
     var botonReiniciar = document.getElementById("botonReiniciar");
 
+    var contenedorMensajes = document.getElementById("mensajeVictoria");
+    var iconoMensajeVictoria = document.getElementById("iconoMensajeVictoria");
+
+    var instrucciones = document.getElementById("instrucciones");
+
+    var listaTiradas = document.getElementById("listaTiradas");
+    var lista = "";
+
     var ranuraMonedas = document.getElementById("ranuraMonedas");
 
     var slot1 = document.getElementById("slot1");
@@ -16,6 +24,10 @@ window.onload = function() {
     var slot3 = document.getElementById("slot3");
 
     var bocadillo = document.getElementById("bocadillo");
+
+
+    //Empieza el juego ----------------------------------------------------
+    iconoMensajeVictoria.style.display = "none";
 
     var monedas = 0;
 
@@ -52,14 +64,9 @@ window.onload = function() {
         alert(monedas);
     }, false);
 
+
     //Accion al pulsar el boton de reiniciar
-    botonReiniciar.addEventListener("click", function(){
-        monedas = 0;
-        ranuraMonedas.removeAttribute("disabled");
-        botonTirar.setAttribute("disabled", "");
-        botonSalir.setAttribute("disabled", "");
-        bocadillo.style.backgroundImage = "url('imagenes/bocadilloComic.png')";
-    }, false);
+    botonReiniciar.addEventListener("click", reiniciar, false);
 
 
     //Accion al pulsar la ranura para monedas
@@ -68,7 +75,7 @@ window.onload = function() {
         aux = window.prompt("¿Con cuántas monedas quieres empezar?");
         monedas = parseInt(aux, 10);
 
-        while (Number.isInteger(monedas) == false){
+        while (Number.isInteger(monedas) == false || monedas < 0){
             alert("El valor introducido debe ser un entero positivo");
 
             aux = window.prompt("¿Con cuántas monedas quieres empezar?");
@@ -79,12 +86,14 @@ window.onload = function() {
         botonTirar.removeAttribute("disabled");
         botonSalir.removeAttribute("disabled");
         bocadillo.style.backgroundImage = "none";
-
-
+        listaTiradas.style.display = "block";
+        instrucciones.style.display = "none";
     }, false);
 
 
     function jugar() {
+
+        monedas -= 1;
 
         let numero1 = random();
         let numero2 = random();
@@ -92,6 +101,30 @@ window.onload = function() {
 
         cargarSlots(numero1, numero2, numero3);
         comprobar(numero1, numero2, numero3);
+
+        setTimeout(function() {
+            if(monedas <= 0){
+                alert("Te has quedado sin monedas");
+                reiniciar();
+            }
+        }, 200);
+
+        
+    }
+
+
+
+    function reiniciar(){
+        monedas = 0;
+
+        lista = "";
+        listaTiradas.innerHTML = lista;
+        ranuraMonedas.removeAttribute("disabled");
+        botonTirar.setAttribute("disabled", "");
+        botonSalir.setAttribute("disabled", "");
+        bocadillo.style.backgroundImage = "url('imagenes/bocadilloComic.png')";
+        listaTiradas.style.display = "none";
+        instrucciones.style.display = "block";
     }
 
 
@@ -109,31 +142,48 @@ window.onload = function() {
         if (numero1 == 6 && numero2 == 6 && numero3 == 6) {
             monedas += 10; //tres zanahorias
             mensajePremio(6);
+            lista = "<p>-1; TRIPLE ZANAHORIA: Monedas +10</p>" + lista;
+            listaTiradas.innerHTML = lista;
         }
 
         else if (numero1 == numero2 && numero2 == numero3) {
             monedas += 5; //tres hortalizas iguales
             mensajePremio(5);
+            lista = "<p>-1; TRIPLE HORTALIZA: Monedas +5</p>" + lista;
+            listaTiradas.innerHTML = lista;
         }
 
         else if ((numero1 == 6 && numero2 == 6) || (numero1 == 6 && numero3 == 6) || (numero2 == 6 && numero3 == 6)) {
-            monedas += 4; //dos zanahorias
+            monedas = 4; //dos zanahorias
             mensajePremio(4);
+            lista += "<p>-1; DOBLE ZANAHORIA: Monedas 4</p>" + lista;
+            listaTiradas.innerHTML = lista;
         }
 
         else if ((numero1 == 6 || numero2 == 6 || numero3 == 6) && (numero1 == numero2 || numero1 == numero3 || numero2 == numero3)) {
-            monedas += 3; //una zanahoria y dos hortalizas iguales
+            monedas = 3; //una zanahoria y dos hortalizas iguales
             mensajePremio(3);
+            lista = "<p>-1; DOBLE HORTALIZA Y ZANAHORIA: Monedas +3</p>" + lista;
+            listaTiradas.innerHTML = lista;
         }
 
         else if (numero1 == numero2 || numero1 == numero3 || numero2 == numero3) {
             monedas += 2; //dos hortalizas iguales
             mensajePremio(2);
+            lista = "<p>-1; DOBLE HORTALIZA: Monedas +2</p>" + lista;
+            listaTiradas.innerHTML = lista;
         }
 
         else if (numero1 == 6 || numero2 == 6 || numero3 == 6) {
             monedas += 1; //una zanahoria
             mensajePremio(1);
+            lista = "<p>-1; ZANAHORIA: Monedas +1</p>" + lista;
+            listaTiradas.innerHTML = lista;
+        }
+
+        else{
+            lista = "<p>-1</p>" + lista;
+            listaTiradas.innerHTML = lista;
         }
     }
 
@@ -157,6 +207,11 @@ window.onload = function() {
     //funciones para los mensajes
     function mensajePremio(opcion){
 
+        let rutaImagenFondo = "";
+
+        listaTiradas.style.display = "none";
+        iconoMensajeVictoria.style.display = "flex";
+
         switch(opcion){
 
             case 1:
@@ -164,6 +219,7 @@ window.onload = function() {
                 iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoria/iconozana.png')"
                 botonTirar.setAttribute("disabled", "");
                 iconoMensajeVictoria.style.color = "black";
+                rutaImagenFondo = "1zanahoria/fondozana2.jpg";
                 break;
 
             case 2:
@@ -171,6 +227,7 @@ window.onload = function() {
                 iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/2hortalizas/icono2hortalizas.png')"
                 botonTirar.setAttribute("disabled", "");
                 iconoMensajeVictoria.style.color = "white";
+                rutaImagenFondo = "2hortalizas/fondo2horta2.jpg";
                 break;
 
             case 3:
@@ -178,6 +235,7 @@ window.onload = function() {
                 iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/1zanahoriay2hortalizas/iconozana2horta.png')"
                 botonTirar.setAttribute("disabled", "");
                 iconoMensajeVictoria.style.color = "white";
+                rutaImagenFondo = "1zanahoriay2hortalizas/fondo21zana2horta.jpg";
                 break;
 
             case 4:
@@ -185,6 +243,7 @@ window.onload = function() {
                 iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/2zanahorias/icono2zana.png')"
                 botonTirar.setAttribute("disabled", "");
                 iconoMensajeVictoria.style.color = "black";
+                rutaImagenFondo = "2zanahorias/fondo2zana2.jpg";
                 break;
 
             case 5:
@@ -192,6 +251,7 @@ window.onload = function() {
                 iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/3hortalizas/icono3hortalizas.png')"
                 botonTirar.setAttribute("disabled", "");
                 iconoMensajeVictoria.style.color = "black";
+                rutaImagenFondo = "3hortalizas/fondo3horta2.jpg";
                 break;
 
             case 6:
@@ -199,21 +259,22 @@ window.onload = function() {
                 iconoMensajeVictoria.style.backgroundImage = "url('imagenes/mensajesVictoria/3zanahorias/estrellatriplezanahoria.png')"
                 botonTirar.setAttribute("disabled", "");
                 iconoMensajeVictoria.style.color = "black";
+                rutaImagenFondo = "3zanahorias/fondo2triplezanahoria.jpg";
                 break;
 
         }
 
 
-        contenedorMensajes.animate([
+        var animacion1 = contenedorMensajes.animate([
             // keyframes
-            { backgroundImage: "url('imagenes/mensajesVictoria/1zanahoriay2hortalizas/fondo21zana2horta.jpg')" }
+            { backgroundImage: "url('imagenes/mensajesVictoria/" + rutaImagenFondo + "')" }
         ], { 
             duration: 100,
             iterations: 50,
             direction: "alternate-reverse"
         });
 
-        iconoMensajeVictoria.animate([
+        var animacion2 = iconoMensajeVictoria.animate([
             // keyframes
             { backgroundSize: "0px 0px"},
             { backgroundSize: "120px 120px" }
@@ -232,6 +293,8 @@ window.onload = function() {
             iconoMensajeVictoria.innerHTML = "";
             contenedorMensajes.style.backgroundImage = "none";
             botonTirar.removeAttribute("disabled");
+            listaTiradas.style.display = "block";
+            iconoMensajeVictoria.style.display = "none";
         }, 5000);
 
     }
